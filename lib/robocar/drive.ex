2534e994b7d2426c6510424070f3a699d6b2f0e8
Adam Pointer <adam.pointer@gmx.com>
2021-09-03 21:01:35 +0100

    client functions

--- lib/robocar/drive.ex
+++ lib/robocar/drive.ex
@@ -10,8 +10,20 @@ defmodule RoboCar.Drive do
 
   @pwm_frequency_hz 1000
 
+  def start() do
+    GenServer.start_link(__MODULE__, :ok)
+  end
+
+  def forwards(pid, power_pct) do
+    GenServer.cast(pid, {:forwards, power_pct})
+  end
+
+  def stop(pid) do
+    GenServer.cast(pid, :stop)
+  end
+
   @impl true
-  def init(:ok) do
+  def init(_arg) do
     with :ok = GPIO.pin_mode(left_motor_direction_a(), :output),
          :ok = GPIO.pin_mode(left_motor_direction_b(), :output),
          :ok = GPIO.pin_mode(right_motor_direction_a(), :output),
@@ -23,26 +35,6 @@ defmodule RoboCar.Drive do
 
   @impl true
   def handle_cast({:forwards, power_pct}, drive) do
-    forwards(drive, power_pct)
-  end
-
-  @impl true
-  def handle_cast(:stop, drive) do
-    stop(drive)
-  end
-
-  @impl true
-  def terminate(_reason, drive) do
-    IO.puts("terminate")
-    stop(drive)
-
-    GPIO.pin_release(left_motor_direction_a())
-    GPIO.pin_release(left_motor_direction_b())
-    GPIO.pin_release(right_motor_direction_a())
-    GPIO.pin_release(right_motor_direction_b())
-  end
-
-  def forwards(drive, power_pct) do
     PWM.configure!(drive.left_power, @pwm_frequency_hz, power_pct) |> PWM.enable()
     PWM.configure!(drive.right_power, @pwm_frequency_hz, power_pct) |> PWM.enable()
 
@@ -54,7 +46,8 @@ defmodule RoboCar.Drive do
     end
   end
 
-  def stop(drive) do
+  @impl true
+  def handle_cast(:stop, drive) do
     PWM.disable(drive.left_power)
     PWM.disable(drive.right_power)
 
@@ -64,5 +57,16 @@ defmodule RoboCar.Drive do
          :ok = GPIO.digital_write(right_motor_direction_b(), :low) do
       {:noreply, drive}
     end
+  end
+
+  @impl true
+  def terminate(_reason, drive) do
+    PWM.disable(drive.left_power)
+    PWM.disable(drive.right_power)
+
+    GPIO.pin_release(left_motor_direction_a())
+    GPIO.pin_release(left_motor_direction_b())
+    GPIO.pin_release(right_motor_direction_a())
+    GPIO.pin_release(right_motor_direction_b())
   end
 end
